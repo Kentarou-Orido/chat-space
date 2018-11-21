b66722b5da49938f8f6b9c2d9b86a946404f6f27
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2018-11-22 02:52:02 +0900

    Message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(function() {
+    function buildHTML(message){
+    if (message.body.match(".+")){
+      var html = `<div class= "right-main__chat-main__message">
+                    <div class= "right-main__chat-main__message__name">
+                      ${message.name}
+                    </div>
+                    <div class= "right-main__chat-main__message__time">
+                      ${message.date}
+                    </div>
+                    <div class= "right-main__chat-main__message__body">
+                      ${message.body}
+                    </div>`
+      }else{
+        var html = `<div class= "right-main__chat-main__message">
+                      <div class= "right-main__chat-main__message__name">
+                        ${message.name}
+                      </div>
+                      <div class= "right-main__chat-main__message__time">
+                        ${message.date}
+                      </div>
+                      <div class= "right-main__chat-main__message__body"></div>
+                      <img class= "right-main__chat-main__message__image" src= ${message.image.url}>
+                    </div>`
+      }
+    return html;
+  }
+  $('#js-form').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    jqxhr = $.ajax({
+      url: $(this).attr('action'),
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function(data) {
+        var html = buildHTML(data);
+        $('.right-main__chat-main').append(html);
+        $('.right-main__chat-main').scrollTop($(".right-main__chat-main")[0].scrollHeight);
+    })
+      .fail(function() {
+        alert('error');
+      });
+    return false;
+  });
+});
